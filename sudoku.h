0d9decef71161f0795baba787076bf241b7f8f67
Narek <narek.ikhtiaryan.ni@gmail.com>
2021-06-12 15:08:05 -0700

    add result array, and constructor for initialized new array

--- sudoku.h
+++ sudoku.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -9,10 +10,16 @@ using std::string;
 using std::stoi;
 using std::endl;
 using std::vector;
+using std::string;
+
+void validate_range(int number);
+void validate_number(string str);
+
 
 class sudoku {
   public:
     sudoku();
+    sudoku(int grid[][9]);
 
     void enterDigits();
     bool is_possible_number(int y, int x, int number);
@@ -20,5 +27,6 @@ class sudoku {
     const static int size = 9;
     void print();
     int grid[9][9];
+    int result[9][9];
     bool is_valid_sudoku_input(int grid[][size], int size);
 };
